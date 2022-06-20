195077c4c7ff4255b093f96572fcf3d27660b00e
Bienn <biennviquiera@gmail.com>
2022-06-20 15:07:21 -0700

    project commit

---
+++ Chirpy/Chirpy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Chirpy
+//
+//  Created by Bienn Viquiera on 6/20/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Chirpy/Chirpy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Chirpy
+//
+//  Created by Bienn Viquiera on 6/20/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
