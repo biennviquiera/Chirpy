56a72e255dc9a5cfd32b888179caab5865c18da2
Bienn <biennviquiera@gmail.com>
2022-06-23 15:43:20 -0700

    details view auto layout configured

--- twitter/View Controllers/DetailsViewController.h
+++ twitter/View Controllers/DetailsViewController.h
@@ -12,7 +12,18 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *userImage;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+@property (weak, nonatomic) IBOutlet UILabel *userHandle;
+@property (weak, nonatomic) IBOutlet UILabel *tweetText;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property (weak, nonatomic) IBOutlet UILabel *retweetCount;
+@property (weak, nonatomic) IBOutlet UIButton *favButton;
+@property (weak, nonatomic) IBOutlet UILabel *favCount;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
 @property (nonatomic, weak) TweetCell *passedTweet;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *replyButton;
 @end
 
 NS_ASSUME_NONNULL_END
