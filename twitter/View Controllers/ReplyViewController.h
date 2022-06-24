67007d677ea43be471343c14fe65c7e09d4c0e42
Bienn <biennviquiera@gmail.com>
2022-06-24 14:06:13 -0700

    linked replyVC storyboard to code

--- twitter/View Controllers/ReplyViewController.h
+++ twitter/View Controllers/ReplyViewController.h
@@ -11,6 +11,13 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ReplyViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *userImage;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+@property (weak, nonatomic) IBOutlet UILabel *userHandle;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property (weak, nonatomic) IBOutlet UILabel *quotedTweet;
+@property (weak, nonatomic) IBOutlet UITextView *replyField;
+@property (weak, nonatomic) IBOutlet UILabel *replyingToLabel;
 
 @end
 
